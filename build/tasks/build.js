234d6df324f6b518a17223094f44be32a372860d
Sven van de Scheur <svenvandescheur@gmail.com>
2016-06-17 16:06:38 +0200

    Minor refactor

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -4,6 +4,7 @@ var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 var paths = require('../paths');
 
+
 var jsName = (paths.packageName + '.js').toLowerCase();
 
 gulp.task('build', ['clean'], function () {
